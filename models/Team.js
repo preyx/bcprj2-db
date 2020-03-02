3d92127662ff2d01c762fa9a1dfc9a36714dc5ef
fbuitron <franciscobuitron21@gmail.com>
2020-03-02 14:25:49 -0800

    added Team model

---
+++ models/Team.js
@@ -0,0 +1,32 @@
+const { Model, STRING } = require('sequelize')
+
+class Team extends Model { }
+
+Team.init({
+  pokemon1: {
+    type: STRING,
+    allowNull: false
+  },
+  pokemon2: {
+    type: STRING,
+    allowNull: false
+  },
+  pokemon3: {
+    type: STRING,
+    allowNull: false
+  },
+  enemy1: {
+    type: STRING,
+    allowNull: false
+  },
+  enemy2: {
+    type: STRING,
+    allowNull: false
+  },
+  enemy3: {
+    type: STRING,
+    allowNull: false
+  }
+}, { sequelize: require('../config'), modelName: 'team' })
+
+module.exports = Team
