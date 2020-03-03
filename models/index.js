6651d43e7cf6f0d0877dc0a39a21bcc3cf1ae39c
pinheadrarry <richarddinh@gmail.com>
2020-03-03 14:57:13 -0800

    created Pokemon.js model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const User = require('./User.js')
 const Team = require('./Team.js')
+const Pokemon = require('./Pokemon.js')
 
 User.hasMany(Team)
 Team.belongsTo(User)
 
-module.exports = { User, Team }
+module.exports = { User, Team, Pokemon }
