5506854095d33aa6bf53faf62828416ee3fdb751
pinheadrarry <richarddinh@gmail.com>
2020-03-03 11:06:14 -0800

    wrote Team Routes

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -1,5 +1,32 @@
 const router = require('express').Router()
-const {Team} = require('../models')
+const Model = require('***ModelPath')
 
+// GET all items
+router.get('/team', (req, res) => {
+  Model.findAll()
+    .then(result => res.json(result))
+    .catch(e => console.error(e))
+})
+
+// POST an item
+router.post('/team', (req, res) => {
+  Model.create(req.body)
+    .then(_ => console.log('Created!'))
+    .catch(e => console.error(e))
+})
+
+// PUT an item
+router.put('/team', (req, res) => {
+  Model.update({ req.body.*** }, { where: { *** SearchCondition *** } })
+    .then(() => console.log('Updated!'))
+    .catch(e => console.error(e))
+})
+
+// DELETE an item
+router.delete('/team/:id', (req, res) => {
+  Model.destroy({ where: { id: parseInt(req.params.id) } })
+    .then(() => console.log('Deleted!'))
+    .catch(e => console.error(e))
+})
 
 module.exports = router
