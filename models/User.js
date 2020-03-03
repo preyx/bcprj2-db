0932cf34169c9d8ce5e314ce0b87fe740a1b22ed
fbuitron <franciscobuitron21@gmail.com>
2020-03-03 10:52:20 -0800

    added User routes: create, update, get, + destroy

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
-const {Model, STRING} = require('sequelize')
+const { Model, STRING } = require('sequelize')
 
-class User extends Model {}
+class User extends Model { }
 
 User.init({
   username: {
     type: STRING,
     allowNull: false
   }
-}, {sequelize: require('../config'), modelName: 'user'})
+}, { sequelize: require('../config'), modelName: 'user' })
 
 module.exports = User
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// app.use(require('./routes'))
+app.use(require('./routes'))
 
-app.listen(process.env.PORT || 3000)
+require('./config').sync()
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(e => console.error(e))
