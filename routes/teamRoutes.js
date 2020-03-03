10185e0b77a340886fdab51dbb6b4dfb2f5337de
fbuitron <franciscobuitron21@gmail.com>
2020-03-03 12:27:03 -0800

    pulled teamRoutes from richard

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -1,5 +1,32 @@
 const router = require('express').Router()
+const {Team} = require('../models')
 
+// GET all items
+router.get('/teams', (req, res) => {
+  Team.findAll()
+    .then(team => res.json(team))
+    .catch(e => console.error(e))
+})
 
+// POST an item
+router.post('/teams', (req, res) => {
+  Team.create(req.body)
+    .then(() => res.sendStatus(200))
+    .catch(e => console.error(e))
+})
+
+// PUT an item
+router.put('/teams/:id', (req, res) => {
+  Team.update(req.body, { where: {id: req.params.id}  })
+    .then(() => res.sendStatus(200))
+    .catch(e => console.error(e))
+})
+
+// DELETE an item
+router.delete('/teams/:id', (req, res) => {
+  Team.destroy({ where: { id: parseInt(req.params.id) } })
+    .then(() => console.log('Deleted!'))
+    .catch(e => console.error(e))
+})
 
 module.exports = router
