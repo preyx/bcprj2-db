1b923d8aff7927d735c770237aded57dc8ee1f28
pinheadrarry <richarddinh@gmail.com>
2020-03-04 11:26:01 -0800

    wrote queries to have pokemon info in return data

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -1,13 +1,38 @@
 const router = require('express').Router()
-const {Team} = require('../models')
+const {Team, User, Pokemon} = require('../models')
 
 // GET all items
 router.get('/teams', (req, res) => {
-  Team.findAll()
+  Team.findAll({include:[
+    { model: Pokemon, as: 'pokemon1', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'pokemon2', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'pokemon3', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'enemy1', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'enemy2', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'enemy3', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    User
+  ]})
     .then(team => res.json(team))
     .catch(e => console.error(e))
 })
 
+//get one team by id
+router.get('/teams/:id', (req, res) => Team.findOne({
+  where: {
+    id: req.params.id
+  },
+  include: [
+    { model: Pokemon, as: 'pokemon1', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'pokemon2', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'pokemon3', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'enemy1', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'enemy2', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    { model: Pokemon, as: 'enemy3', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+    User
+  ]
+})
+  .then(user => res.json(user))
+  .catch(e => console.error(e)))
 // POST an item
 router.post('/teams', (req, res) => {
   Team.create(req.body)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,39 @@
 const router = require('express').Router()
-const { User, Team } = require('../models')
+const { User, Team, Pokemon } = require('../models')
 
 // GET all users
-router.get('/users', (req, res) => User.findAll({ include: [Team] })
+router.get('/users', (req, res) =>{
+  User.findAll({include: [{
+      model: Team,
+    include: [
+      { model: Pokemon, as: 'pokemon1', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+      { model: Pokemon, as: 'pokemon2', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+      { model: Pokemon, as: 'pokemon3', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+      { model: Pokemon, as: 'enemy1', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+      { model: Pokemon, as: 'enemy2', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] },
+      { model: Pokemon, as: 'enemy3', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] }]
+    }]
+  })
   .then(users => res.json(users))
-  .catch(e => console.error(e)))
+  .catch(e => console.error(e))
+}) 
 
 // GET one user
-router.get('/users/id', (req, res) => User.findOne({
+router.get('/users/:id', (req, res) => User.findOne({
   where: {
     id: req.params.id
   },
-  include: [Team]
+  include: [{
+    model: Team,
+    include: [
+    { model: Pokemon, as: 'pokemon1', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] }, 
+    { model: Pokemon, as: 'pokemon2', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] }, 
+    { model: Pokemon, as: 'pokemon3', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] }, 
+    { model: Pokemon, as: 'enemy1', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] }, 
+    { model: Pokemon, as: 'enemy2', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] }, 
+    { model: Pokemon, as: 'enemy3', attributes: ['name', 'hp', 'attack', 'sp_attack', 'defense', 'sp_defense', 'speed', 'base_total'] }
+  ]
+  }]
 })
   .then(user => res.json(user))
   .catch(e => console.error(e)))
