6288de41e48906d6038cc245b9e63a4452c993b1
fbuitron <franciscobuitron21@gmail.com>
2020-03-04 14:53:22 -0800

    added bad matchups array

--- routes/pokeInfoRoutes.js
+++ routes/pokeInfoRoutes.js
@@ -24,24 +24,36 @@ router.get('/pokemon/matchups/:id', (req, res) => Pokemon.findAll({
   where: {
     id: req.params.id
   },
-  attributes: ['against_bug', 'against_dark', 'against_dragon', 'against_electric', 'against_fairy', 'against_fight', 'against_fire', 'against_flying', 'against_ghost', 'against_grass', 'against_ground', 'against_ice', 'against_normal', 'against_poison', 'against_psychic', 'against_rock', 'against_steel', 'against_water']
+  attributes: ['against_bug', 'against_dark', 'against_dragon', 'against_electric', 'against_fairy', 'against_fight', 'against_fire', 'against_flying', 'against_ghost', 'against_grass', 'against_ground', 'against_ice', 'against_normal', 'against_poison', 'against_psychic', 'against_rock', 'against_steel', 'against_water', 'base_total']
 })
   .then(pokemon => {
     let matchups = pokemon[0].dataValues
     let goodMatchups = []
+    let badMatchups = []
+    let base_total = matchups.base_total
+    delete matchups['base_total']
     for (element in matchups) {
       console.log(`${element}: ${matchups[element]}`)
       if (matchups[element] >= 2) {
         goodMatchups.push(element)
+      }
+      if (matchups[element] < 1) {
+        badMatchups.push(element)
       }
     }
     goodMatchups = goodMatchups.map(element => {
       element = element.split('_')
       return element[1]
     })
-    console.log(goodMatchups)
+    badMatchups = badMatchups.map(element => {
+      element = element.split('_')
+      return element[1]
+    }) 
     Pokemon.findAll({
       where: {
+        base_total: {
+          [Op.gte]: base_total - 100
+        },
         [Op.or]: [
           {type1: goodMatchups},
           {type2: goodMatchups}
