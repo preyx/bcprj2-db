977893c4da0bf364909656b386c49ccd6641ae07
jordan <jordanscheftic@yahoo.com>
2020-03-05 13:03:46 -0800

    got layout for home jsx done

---
+++ views/list.jsx
@@ -0,0 +1,39 @@
+<div class="container-fluid">
+  <div class="row" id="row">
+    <!-- ~~~~~~~~~~~~~~~~~~~~~~~ col 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+      <div class="col col-sm-6" id="col">
+      <h1>Enemy Team</h1>
+      <div class="flexContainer w-100">
+        <div class="card p-1">
+          <img src="http://img.pokemondb.net/sprites/black-white/anim/normal/snorlax.gif"
+            class="card-img-top img-fluid" alt="..." />
+            <h1>Title</h1>
+            <div class="card-body p-0">
+              <ul class="list-group list-group-flush">
+                <!-- type 1 -->
+                <li class="list-group-item">
+                  grass
+                </li>
+                <!-- type 2 -->
+                <li class="list-group-item">
+                  poison
+                </li>
+                <!-- base hp -->
+                <li class="list-group-item">
+                  45
+                </li>
+                <!-- base att -->
+                <li class="list-group-item">
+                  65
+                </li>
+                <!-- base def -->
+                <li class="list-group-item">
+                  49
+                </li>
+                <!-- base spec -->
+                <!-- <li>
+
+                </li> -->
+              </ul>
+            </div>
+          </div>
