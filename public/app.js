d697b977badfec2005c22d0494dbe5af41f62b01
pinheadrarry <richarddinh@gmail.com>
2020-03-05 11:14:07 -0800

    started on front end js

---
+++ public/app.js
@@ -0,0 +1,22 @@
+//global variable to house userId
+let userId
+
+document.addEventListener('click', event => {
+  target = event.target
+  //checks if the event is a button
+  if(target.nodeName ==='BUTTON'){
+    event.preventDefault()
+    //checks if the create account button is clicked
+    if(target.id ==='create***'){
+      
+    }
+    //checks if the sign in button is clicked
+    else if(target.id==='signIn***'){
+
+    }
+    //checks if the sign out button is 
+    else if(target.id==='signOut***'){
+
+    }
+  }
+})
