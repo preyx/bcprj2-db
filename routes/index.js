892da260a75c2b3bd376ea2a9c96f34790c2a02c
Bryan Y <xpreyx@gmail.com>
2020-03-09 11:15:57 -0700

    auto-formatted /models and /routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,4 @@ router.use('/api', require('./userRoutes.js'))
 router.use('/api', require('./teamRoutes.js'))
 router.use('/api', require('./pokeInfoRoutes.js'))
 
-//add routes for front end views
-
 module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const {join} = require('path')
+const { join } = require('path')
 const sequelize = require('./config')
 
 const app = express()
@@ -19,6 +19,5 @@ app.get('/', (req, res) => {
 app.use(require('./routes'))
 
 sequelize.sync()
-.then(() => app.listen(process.env.PORT || 3000) )
-.catch(error => console.error(error))
-
+  .then(() => app.listen(process.env.PORT || 3000))
+  .catch(error => console.error(error))
